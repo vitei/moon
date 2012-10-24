177b399d10fbe80ef638051953a1607c78b9f5bc
Ben Sherratt <ben@vitei.com>
2012-10-24 10:52:32 +0900

    Added an error function that doesn't take a line number.

--- sources/error.cpp
+++ sources/error.cpp
@@ -2,6 +2,11 @@
 #include "error.h"
 
 
+void emitError(const char *error)
+{
+	std::cerr << "Error: " << error << std::endl;
+}
+
 void emitError(unsigned int lineNum, const char *error)
 {
 	std::cerr << "Error on line #" << lineNum << ": " << error << std::endl;
--- sources/error.h
+++ sources/error.h
@@ -1,6 +1,7 @@
 #ifndef ERROR_H
 #define ERROR_H
 
+extern void emitError(const char *error);
 extern void emitError(unsigned int lineNum, const char *error);
 
 #endif
