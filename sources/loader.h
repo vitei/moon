876a6fc01d97bf3c8423b7b61fb216a29b0c5dbb
Ben Sherratt <ben@vitei.com>
2012-11-20 15:49:51 +0900

    Functions to convert use name to filename.

--- sources/loader.h
+++ sources/loader.h
@@ -5,6 +5,11 @@ namespace loader
 {
 	void addClassDirectory(const char *directory);
 	void addIncludeDirectory(const char *directory);
+
+	void classNameToFilename(char *filename, const char *className);
+
+	FILE *classFile(const char *filename);
+	FILE *includeFile(const char *filename);
 }
 
 #endif
