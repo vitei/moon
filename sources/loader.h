11bb9086e45cc4e61ed0d8406fa7bcb10b62d066
Ben Sherratt <ben@vitei.com>
2012-11-21 15:39:27 +0900

    The current working directory is used for file path generation.
    The directory of files in subdirectories is used as the current working directory whilst processing that file.

--- sources/loader.h
+++ sources/loader.h
@@ -9,11 +9,14 @@ namespace loader
 	void addUseDirectory(const char *directory);
 	void addIncludeDirectory(const char *directory);
 
+	void pushCWD(const char *directory);
+	void popCWD();
+
 	void useNameToFilename(char *filename, const char *name);
 	void includeNameToFilename(char *filename, const char *name);
 
-	FILE *useFile(const char *filename);
-	FILE *includeFile(const char *filename);
+	FILE *useFile(const char *filename, char *usedFilename = 0);
+	FILE *includeFile(const char *filename, char *usedFilename = 0);
 }
 
 #endif
