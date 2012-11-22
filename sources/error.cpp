5063b9b1dc486019dbbd9c9d2ec7dbe1cde7463b
Ben Sherratt <ben@vitei.com>
2012-11-22 15:03:01 +0900

    Added an error hierarchy.

--- sources/error.cpp
+++ sources/error.cpp
@@ -3,34 +3,66 @@
 #include "error.h"
 
 
-class ErrorMessage
+class Error
 {
 public:
-	unsigned int mLineNumber;
+	Error(const char *message) : mMessage(std::string(message)) {}
+
+	virtual void output()
+	{
+		std::cerr << "\033[1;31mError\033[0m: " << mMessage << std::endl;
+	}
+
+protected:
 	std::string mMessage;
 };
 
-static std::vector<ErrorMessage> sErrorList;
+class FileError : public Error
+{
+public:
+	FileError(const char *filename, const char *message) : Error(message), mFilename(std::string(filename)) {}
 
-//void emitError(const char *error)
-//{
-//	std::cerr << "Error: " << error << std::endl;
-//}
+	virtual void output()
+	{
+		Error::output();
+		std::cerr << "\tFile: " << mFilename << std::endl;
+	}
+
+protected:
+	std::string mFilename;
+};
+
+class SyntaxError : public FileError
+{
+public:
+	SyntaxError(unsigned int lineNumber, const char *filename, const char *message) : FileError(filename, message), mLineNumber(lineNumber) {}
+
+	virtual void output()
+	{
+		FileError::output();
+		std::cerr << "\tLine #" << mLineNumber << std::endl;
+	}
+
+protected:
+	unsigned int mLineNumber;
+};
+
+static std::vector<Error *> sErrorList;
+
+void error::enqueue(const char *message)
+{
+	sErrorList.push_back(new Error(message));
+}
 
 void error::enqueue(unsigned int lineNumber, const char *message)
 {
-	ErrorMessage errorMessage;
-
-	errorMessage.mLineNumber = lineNumber;
-	errorMessage.mMessage = std::string(message);
-
-	sErrorList.push_back(errorMessage);
+	sErrorList.push_back(new SyntaxError(lineNumber, "", message));
 }
 
 void error::output()
 {
-	for(std::vector<ErrorMessage>::iterator i = sErrorList.begin(), end = sErrorList.end(); i != end; ++i)
+	for(std::vector<Error *>::iterator i = sErrorList.begin(), end = sErrorList.end(); i != end; ++i)
 	{
-		std::cerr << "Error on line #" << i->mLineNumber << ": " << i->mMessage << std::endl;
+		(*i)->output();
 	}
 }
--- sources/error.h
+++ sources/error.h
@@ -3,6 +3,7 @@
 
 namespace error
 {
+	void enqueue(const char *message);
 	void enqueue(unsigned int lineNumber, const char *message);
 	void output();
 }
