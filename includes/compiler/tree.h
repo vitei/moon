661b1b59f7c43959ade3641461c904ea2d5ac451
Ben Sherratt <ben@vitei.com>
2012-12-12 19:16:24 +0900

    Moved the includes.

---
+++ includes/compiler/tree.h
@@ -0,0 +1,15 @@
+#ifndef COMPILER_TREE_H
+#define COMPILER_TREE_H
+
+#include "compiler/tree/expression.h"
+#include "compiler/tree/function.h"
+#include "compiler/tree/program.h"
+#include "compiler/tree/scope.h"
+#include "compiler/tree/scoping.h"
+#include "compiler/tree/state.h"
+#include "compiler/tree/statement.h"
+#include "compiler/tree/type.h"
+#include "compiler/tree/udt.h"
+#include "compiler/tree/use.h"
+
+#endif
