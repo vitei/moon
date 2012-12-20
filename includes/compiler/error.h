9e78128ad3cf6e1788ce2f481d20f7362219b2df
Ben Sherratt <ben@vitei.com>
2012-12-20 13:12:33 +0900

    Conflict error.

--- includes/compiler/error.h
+++ includes/compiler/error.h
@@ -9,6 +9,7 @@ namespace error
 	unsigned int count();
 	void enqueue(const std::string &message);
 	void enqueue(tree::Node::Location &location, const std::string &message);
+	void enqueue(tree::Node::Location &conflictLocation, tree::Node::Location &location, const std::string &message);
 	void output();
 }
 
