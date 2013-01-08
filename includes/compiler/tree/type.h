b3683dc8f7c3508b272f6439e1edede415547feb
Ben Sherratt <ben@vitei.com>
2013-01-08 19:09:34 +0900

    Types now have an internal number to identify them.
    This is because you can have multiple instances of bool etc. but still need to identify them as the same type.

--- includes/compiler/tree/type.h
+++ includes/compiler/tree/type.h
@@ -11,21 +11,39 @@ namespace tree
 	class Type : public Node
 	{
 	protected:
-		Type() { /* Abstract class */ }
+		enum Internal
+		{
+			TYPE_BOOL,
+			TYPE_INT,
+			TYPE_FLOAT,
+			TYPE_STRING,
+			TYPE_UDT
+		};
+
+		Type(Internal type) : mType(type) { /* Abstract class */ }
+
+	private:
+		Internal mType;
 	};
 
 	/* ---- ONLY CONCRETE CLASSES BELOW HERE ---- */
 
 	class Bool : public Type
 	{
+	public:
+		Bool() : Type(tree::Type::TYPE_BOOL) {}
 	};
 
 	class Int : public Type
 	{
+	public:
+		Int() : Type(tree::Type::TYPE_INT) {}
 	};
 
 	class Float : public Type
 	{
+	public:
+		Float() : Type(tree::Type::TYPE_FLOAT) {}
 	};
 
 	class String : public Type
@@ -33,7 +51,7 @@ namespace tree
 	public:
 		static const unsigned int STRING_DEFAULT_MAX_SIZE = 256;
 
-		String(unsigned int maxSize = STRING_DEFAULT_MAX_SIZE) : mMaxSize(maxSize) {}
+		String(unsigned int maxSize = STRING_DEFAULT_MAX_SIZE) : Type(tree::Type::TYPE_STRING), mMaxSize(maxSize) {}
 
 		unsigned int getMaxSize()
 		{
