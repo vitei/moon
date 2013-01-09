d23816b07391d1821987f57b76088c8e43d4f2fd
Ben Sherratt <ben@vitei.com>
2013-01-09 10:18:00 +0900

    If the identifier cannot be resolved it must be returned.

--- sources/compiler/operations/resolve_identities.cpp
+++ sources/compiler/operations/resolve_identities.cpp
@@ -11,21 +11,20 @@ void operation::ResolveIdentities::run(tree::Program *program)
 
 tree::Node *operation::ResolveIdentities::restructure(tree::Identifier *identifier)
 {
-	tree::Node *r = NULL;
+	tree::Node *r = identifier;
 
 	LOG("ResolveIdentities::restructure::Identifier");
 
 	try
 	{
 		r = getCurrentScope()->findIdentity(identifier);
+		delete identifier;
 	}
 	catch(tree::Scope::NotFoundException &e)
 	{
 		std::string error = "The identifier \"" + identifier->getName() + "\" does not exist";
 		error::enqueue(identifier->getLocation(), error);
 	}
-
-	delete identifier;
 
 	return r;
 }
