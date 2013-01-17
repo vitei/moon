67ae60308c93b365e37551a3123db7e72c71515b
Ben Sherratt <ben@vitei.com>
2013-01-17 18:32:47 +0900

    Re-structured C generator.

--- includes/compiler/generators/c.h
+++ includes/compiler/generators/c.h
@@ -12,80 +12,111 @@ namespace generator
 	class C : public Generator
 	{
 	public:
-		C() : mDepth(0) {}
-
 		virtual void run(std::ostream &output, tree::Program *program);
+
+		class Printer
+		{
+		public:
+			Printer() : mOutput(NULL), mDepth(0) {}
+
+			void init(std::ostream &output, tree::Program *program)
+			{
+				mOutput = &output;
+				mProgram = program;
+				mStructName = "moon_" + program->getName() + "Data";
+			}
+
+			void dispatch(tree::Node *node);
+
+			void output(tree::Scope *scope);
+			void output(tree::Function *function);
+
+			void output(tree::Identity *identity);
+			void output(tree::Reference *reference);
+			void output(tree::Cast *cast);
+			void output(tree::DirectAccess *directAccess);
+			void output(tree::MessageAccess *messageAccess);
+			void output(tree::ArrayAccess *arrayAccess);
+			void output(tree::FunctionCall *functionCall);
+			void output(tree::NullReference *nullReference);
+			void output(tree::BoolLiteral *boolLiteral);
+			void output(tree::IntLiteral *intLiteral);
+			void output(tree::FloatLiteral *floatLiteral);
+			void output(tree::StringLiteral *stringLiteral);
+			void output(tree::Assign *assign);
+			void output(tree::LogicalOr *logicalOr);
+			void output(tree::LogicalAnd *logicalAnd);
+			void output(tree::Or *orExpression);
+			void output(tree::Xor *xorExpression);
+			void output(tree::And *andExpression);
+			void output(tree::Equal *equal);
+			void output(tree::Unequal *unequal);
+			void output(tree::LessThan *lessThan);
+			void output(tree::LessEqual *lessEqual);
+			void output(tree::GreaterThan *greaterThan);
+			void output(tree::GreaterEqual *greaterEqual);
+			void output(tree::Add *add);
+			void output(tree::Subtract *subtract);
+			void output(tree::Multiply *multiply);
+			void output(tree::Divide *divide);
+			void output(tree::Modulus *modulus);
+			void output(tree::LogicalNot *logicalNot);
+			void output(tree::Not *notExpression);
+			void output(tree::Minus *minus);
+
+			void output(tree::Execute *execute);
+			void output(tree::Return *returnExpression);
+
+			void output(tree::Node *node)
+			{
+#ifdef DEBUG
+				node->printNode();
+				ERROR("Should never reach here");
+#endif
+			}
+
+			void outputEOS();
+			void outputExtern(tree::TypedIdentity *typedIdentity);
+			void outputDeclaration(tree::TypedIdentity *typedIdentity);
+
+			void outputVariablesBegin();
+			void outputVariablesEnd();
+
+			void outputNewBegin();
+			void outputNewEnd();
+
+			void outputPragma(std::string pragma);
+
+			void outputRaw(std::string data);
+			void outputTabs();
+
+			void increaseDepth()
+			{
+				mDepth++;
+			}
+
+			void decreaseDepth()
+			{
+				mDepth--;
+			}
+
+		private:
+			std::ostream *mOutput;
+			tree::Program *mProgram;
+			std::string mStructName;
+			unsigned int mDepth;
+		};
 
 	private:
 		virtual void generate(tree::Program *program);
 
-		virtual void dispatch(tree::Node *node);
+		void mangleNames(tree::Program *program);
+		void outputConstants(tree::Program *program);
+		void outputVariables(tree::Program *program);
+		void outputFunctions(tree::Program *program);
+		void outputNew(tree::Program *program);
 
-		virtual void generate(tree::Scope *scope);
-		virtual void generate(tree::Function *function);
-
-		virtual void generate(tree::Identity *identity);
-	//	virtual void generate(tree::Constant *constant);
-		virtual void generate(tree::Reference *reference);
-		virtual void generate(tree::Cast *cast);
-		virtual void generate(tree::DirectAccess *directAccess);
-		virtual void generate(tree::MessageAccess *messageAccess);
-		virtual void generate(tree::ArrayAccess *arrayAccess);
-		virtual void generate(tree::FunctionCall *functionCall);
-		virtual void generate(tree::NullReference *nullReference);
-		virtual void generate(tree::BoolLiteral *boolLiteral);
-		virtual void generate(tree::IntLiteral *intLiteral);
-		virtual void generate(tree::FloatLiteral *floatLiteral);
-		virtual void generate(tree::StringLiteral *stringLiteral);
-		virtual void generate(tree::Assign *assign);
-		virtual void generate(tree::LogicalOr *logicalOr);
-		virtual void generate(tree::LogicalAnd *logicalAnd);
-		virtual void generate(tree::Or *orExpression);
-		virtual void generate(tree::Xor *xorExpression);
-		virtual void generate(tree::And *andExpression);
-		virtual void generate(tree::Equal *equal);
-		virtual void generate(tree::Unequal *unequal);
-		virtual void generate(tree::LessThan *lessThan);
-		virtual void generate(tree::LessEqual *lessEqual);
-		virtual void generate(tree::GreaterThan *greaterThan);
-		virtual void generate(tree::GreaterEqual *greaterEqual);
-		virtual void generate(tree::Add *add);
-		virtual void generate(tree::Subtract *subtract);
-		virtual void generate(tree::Multiply *multiply);
-		virtual void generate(tree::Divide *divide);
-		virtual void generate(tree::Modulus *modulus);
-		virtual void generate(tree::LogicalNot *logicalNot);
-		virtual void generate(tree::Not *notExpression);
-		virtual void generate(tree::Minus *minus);
-
-		virtual void generate(tree::Execute *execute);
-		virtual void generate(tree::Return *returnExpression);
-
-		virtual void generate(tree::Node *node)
-		{
-#ifdef DEBUG
-			node->printNode();
-			ERROR("Should never reach here");
-#endif
-		}
-
-		void increaseDepth()
-		{
-			mDepth++;
-		}
-
-		void decreaseDepth()
-		{
-			mDepth--;
-		}
-
-		void outputDeclaration(tree::Identity *identity);
-		void outputTabs();
-
-		tree::Scope *mCurrentScope;
-		std::ostream *mOutput;
-		unsigned int mDepth;
-		std::string mStructName;
+		Printer mPrinter;
 	};
 }
 
