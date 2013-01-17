3547f89b9b52ce58a8dbbc2b6cf205025c3ddfac
Ben Sherratt <ben@vitei.com>
2013-01-17 17:01:47 +0900

    Added BooleanBinaryExpression and BooleanUnaryExpression.
    Certain expressions will return booleans.

--- includes/compiler/operations/type_expressions.h
+++ includes/compiler/operations/type_expressions.h
@@ -17,7 +17,9 @@ namespace operation
 		virtual void visit(tree::Access *access);
 		virtual void visit(tree::ArrayAccess *arrayAccess);
 		virtual void visit(tree::BinaryExpression *binaryExpression);
+		virtual void visit(tree::BooleanBinaryExpression *booleanBinaryExpression);
 		virtual void visit(tree::UnaryExpression *unaryExpression);
+		virtual void visit(tree::BooleanUnaryExpression *booleanUnaryExpression);
 		virtual void visit(tree::FunctionCall *functionCall);
 		virtual void visit(tree::Scope *scope);
 		virtual void visit(tree::Return *returnStatement);
