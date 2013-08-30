70b87b03b8c5549d4160c6625f77ed5a9b90853b
Ben Sherratt <ben@vitei.com>
2013-08-30 15:54:13 +0900

    Removed CheckTypes operation.

--- includes/compiler/operations.h
+++ includes/compiler/operations.h
@@ -3,7 +3,6 @@
 
 #include "operations/cast_expressions.h"
 #include "operations/check_typecasting.h"
-#include "operations/check_types.h"
 #include "operations/compute_constants.h"
 #include "operations/infer_types.h"
 #include "operations/remove_dead_nodes.h"
